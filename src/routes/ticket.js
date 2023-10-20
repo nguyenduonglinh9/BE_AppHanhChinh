fbad96960411e0648d8a029ceaf406cd6d9e9b2b
DuongLinh <nguyenduonglinh9@gmail.com>
2023-10-20 10:13:00 +0700

    lấy thông tin 1 vé

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const ticketController = require("../controllers/ticketController");
 const middleWareAuth = require("../middlewares/auth");
 
-router.use("/:slug", middleWareAuth, ticketController.getOne);
 router.use("/create", middleWareAuth, ticketController.createOne);
+router.use("/:slug", middleWareAuth, ticketController.getOne);
 router.use("/", middleWareAuth, ticketController.getAll);
 
 module.exports = router;
