f79dfcce9a9242bad47577855a839af81a882538
DuongLinh <nguyenduonglinh9@gmail.com>
2023-10-25 13:51:57 +0700

    thêm phương thức cập nhật phiếu (cập nhật lần 1)

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ticketController = require("../controllers/ticketController");
 const middleWareAuth = require("../middlewares/auth");
 
-router.use("/update/:slug", middleWareAuth, ticketController.updateOne);
+router.use("/update/:slug", ticketController.updateOne);
 router.use("/create", middleWareAuth, ticketController.createOne);
 router.use("/:slug", ticketController.getOne);
 router.use("/", middleWareAuth, ticketController.getAll);
